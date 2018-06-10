5c2c5cb168433f83ca5d7651903c2f9da2524e28
Rimvydas Zilinskas <rilinskas@yahoo.com>
2018-06-10 22:12:27 +0200

    table display done

--- src/App.js
+++ src/App.js
@@ -5,24 +5,83 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      ships: []
-    }
+      ships: [],
+      loaded: false,
+      keys: []
+    };
+  }
 
+  componentDidMount(){
+    this.getSpaceShips();
   }
 
   getSpaceShips(){
     Axios({
       method: 'get',
       url: 'https://swapi.co/api/starships/',
-      responseType: 'application/jsons',
+      responseType: 'json',
     }).then((response) => {
-      console.log(response.data.results);
+      this.setState({
+        loaded: true,
+        ships: response.data.results
+      });
     });
   }
 
+  eachHeader(header, i){
+    return(
+      <th key={i}>{header}</th>
+    )
+  }
+
+  eachSpaceShiper(spaceShip){
+    return (
+      <tr>
+        <td>{spaceShip.name}</td>
+        <td>{spaceShip.model}</td>
+        <td>{spaceShip.manufacturer}</td>
+        <td>{spaceShip.cost_in_credits}</td>
+        <td>{spaceShip.length}</td>
+        <td>{spaceShip.max_atmosphering_speed}</td>
+        <td>{spaceShip.crew}</td>
+        <td>{spaceShip.passengers}</td>
+        <td>{spaceShip.cargo_capacity}</td>
+        <td>{spaceShip.consumables}</td>
+        <td>{spaceShip.hyperdrive_rating}</td>
+        <td>{spaceShip.MGLT}</td>
+        <td>{spaceShip.starship_class}</td>
+        <td>{spaceShip.pilots.length === 0 ? "" : spaceShip.pilots.length}</td>
+        <td>{spaceShip.films.length === 0 ? "" : spaceShip.films.length}</td>
+        <td>{spaceShip.created.split("T")[0]}</td>
+        <td>{spaceShip.edited.split("T")[0]}</td>
+        <td><a href={spaceShip.url}>Link</a></td>
+      </tr>
+    );
+  }
+
   render(){
-    this.getSpaceShips();
-    return "hello";
+    if(this.state.loaded){
+      let keys = Object.keys(this.state.ships[0]);
+      // this.setState({
+      //   keys: keys
+      // });
+
+      return (
+        <table border="1">
+          <thead>
+            <tr>
+              {keys.map(this.eachHeader)}
+            </tr>
+          </thead>
+          <tbody>
+              {this.state.ships.map(this.eachSpaceShiper)}
+          </tbody>
+        </table>
+      );
+    }
+    else{
+      return "nothing";
+    }
   }
 }
 
